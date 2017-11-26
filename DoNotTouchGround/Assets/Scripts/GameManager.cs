3b7d4f822d3ca9a3c581f96dcd241790dcd50862
Ka Wong <kawong2020@u.northwestern.edu>
2017-11-26 17:25:57 -0600

    Renamed variables, created asteroid spawn location method to return a Vector3 object

--- DoNotTouchGround/Assets/Scripts/GameManager.cs
+++ DoNotTouchGround/Assets/Scripts/GameManager.cs
@@ -6,30 +6,30 @@ public class GameManager : MonoBehaviour {
     // Singleton Design Pattern
     public static GameManager Instance;
 
-    // Modulate type of enemy of spawn
-    [SerializeField] private GameObject m_EnemyPrefab;
+    // Modulate type of asteroid of spawn
+    [SerializeField] private GameObject m_AsteroidPrefab;
 
     // Max enemies for debugging intially ***
-    private const int MAX_ENEMIES = 5;
+    private const int MAX_ASTEROIDS = 5;
 
     // Current amount of enemies in the game
-	private int m_CurrentEnemies = 0;
+	private int m_CurrentAsteroids = 0;
 
-    // Time since the last enemy was spawned
-    private float m_TimeSinceLastEnemy = 0;
+    // Time since the last asteroid was spawned
+    private float m_TimeSinceLastAsteroid = 0;
 
-    // How frequently an enemy should spawn
-    private float m_AddEnemyTime = 10;
+    // How frequently an asteroid should spawn
+    private float m_AddAsteroidTime = 10;
 
-    // Resets the game such that there is only one enemy
+    // Resets the game such that there is only one asteroid
     public void ResetGame()
     {
-        foreach (var e in FindObjectsOfType<Enemy>())
+        foreach (var e in FindObjectsOfType<Asteroid>())
         {
             Destroy(e.gameObject);
         }
-        m_CurrentEnemies = 0;
-        m_TimeSinceLastEnemy = 0;
+        m_CurrentAsteroids = 0;
+        m_TimeSinceLastAsteroid = 0;
         AddEnemy();
     }
 
@@ -47,34 +47,50 @@ public class GameManager : MonoBehaviour {
     // Called once per frame
     void Update () {
         /*
-		 * Increment time since last enemy by the time that has elapsed since
+		 * Increment time since last asteroid by the time that has elapsed since
 		 * the last frame.
 		 */
-        m_TimeSinceLastEnemy += Time.deltaTime;
+        m_TimeSinceLastAsteroid += Time.deltaTime;
 
-        // If the time since the last enemy is less than the spawn interval, do nothing.
-        if (m_TimeSinceLastEnemy <= m_AddEnemyTime) return;
+        // If the time since the last asteroid is less than the spawn interval, do nothing.
+        if (m_TimeSinceLastAsteroid <= m_AddAsteroidTime) return;
 
-        // Otherwise, add an enemy and reset the timer!
-        m_TimeSinceLastEnemy = 0;
+        // Otherwise, add an asteroid and reset the timer!
+        m_TimeSinceLastAsteroid = 0;
         AddEnemy();
     }
 
-    // Return random vector3 location to spawn asteroids
-    private Vector3 randomLocation()
+    // Return random vector3 values to spawn asteroids
+    private Vector3 spawnLocation()
     {
-        // To be done;
+        float x, y, z;
+        // Choose to spawn either in x or y bounds
+        if (Random.Range(0.0f, 1.0f) > 0.5f) // > 0.5 spawn X else spawn Y
+        {
+            x = Random.Range(0.0f, 1.0f) * Screen.width;
+            y = 0.0f;
+            z = 0.0f;
+        }
+        else
+        {
+            x = 0.0f;
+            y = Random.Range(0.0f, 1.0f) * Screen.height;
+            z = 0.0f;
+        }
+        Vector3 location = new Vector3(x, y, z);
+        return location;
+
     }
 
 
-    // Adds an enemy to the game
+    // Adds an asteroid to the game
     private void AddEnemy()
     {
         // If we already have the maximum number of enemies, do nothing.
-        if (m_CurrentEnemies >= MaxEnemies) return;
+        if (m_CurrentAsteroids >= MAX_ASTEROIDS) return;
 
-        // Otherwise, add an enemy to the game.
-        Instantiate(m_EnemyPrefab, new Vector3(0, 2, 0), Quaternion.identity);
-        m_CurrentEnemies += 1;
+        // Otherwise, add an asteroid to the game.
+        Instantiate(m_AsteroidPrefab, spawnLocation(), Quaternion.identity);
+        m_CurrentAsteroids += 1;
     }
 }

b7d253f73cc3519ce1db4aa23e0634d0ff68d439
Sweisian <rswei888@gmail.com>
2017-12-03 19:45:27 -0600

    Added player object

---
+++ DoNotTouchGround/Assets/Scripts/Player_Script.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Script : MonoBehaviour {
+
+    // Maximum speed allowed for an enemy
+    [SerializeField] private float m_MaxSpeed;
+    Vector2 centerPoint = new Vector2(0f, 0f);
+
+    [SerializeField] public float MoveSpeed = 10;
+    [SerializeField] public float RotateSpeed = 40;
+
+    private Rigidbody2D m_RigidBody;
+
+
+    private void Start()
+    {
+
+        m_RigidBody = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    // Amount to Move
+	    float MoveForward = Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
+	    float MoveRotate = Input.GetAxis("Horizontal") * RotateSpeed * Time.deltaTime;
+
+        // Move the player
+        //transform.Translate(Vector2.up * MoveForward);
+	    m_RigidBody.AddRelativeForce(Vector2.up * MoveForward);
+        transform.Rotate(Vector3.back * MoveRotate);
+    }
+}

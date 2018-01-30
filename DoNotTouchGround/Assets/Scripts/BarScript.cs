6c9b8d744ce46cb70ba077d539dafa925b7d8505
Sweisian <rswei888@gmail.com>
2018-01-30 13:27:22 -0600

    Added a heat bar and changed some HUD elements. HUD tuning coming soon

---
+++ DoNotTouchGround/Assets/Scripts/BarScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour
+{
+    private float fillAmount;
+    [SerializeField] private Image content;
+
+    public void BarHandler(float min, float max, float currPercentage)
+    {
+        fillAmount = Mathf.Lerp(min, max, currPercentage);
+        content.fillAmount = fillAmount;
+
+        Debug.Log(min);
+        Debug.Log(max);
+        Debug.Log(currPercentage);
+        Debug.Log(fillAmount);
+        Debug.Log("This is barScript");
+
+    }
+
+   
+}
